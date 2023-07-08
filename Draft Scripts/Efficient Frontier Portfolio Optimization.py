837d2315e568b1350a389416cfde8301b9e565b6
Joe Holden <joemattholden@gmail.com>
2023-07-07 21:21:52 -0500

    Update and rename Efficient Frontier Portfolio Optimization.py to Draft Scripts/Efficient Frontier Portfolio Optimization.py
    reorganizing files

---
+++ Draft Scripts/Efficient Frontier Portfolio Optimization.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import statistics
+
+"""Import portfolio names and plot the efficient frontier. Return optimal weights for various risk tolerances"""
--- Efficient Frontier Portfolio Optimization.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import math
-import statistics
-
-"""Import portfolio names and plot the efficient frontier. Return optimal weights for various risk tolerances"""
